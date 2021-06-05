24e88088c992f48bc1291bb279baf8c182b2859f
Facundo Choque <facundo.choque.silberman@itscordoba.com>
2021-06-05 12:49:49 -0300

    Some functions fixed, added LIST and QUIT

--- clienteTCP_redes2021.py
+++ clienteTCP_redes2021.py
@@ -1,10 +1,12 @@
+
 from socket import * 
+
 
 class Client():
 
     def __init__(self):
         self.client_address = 'localhost'
-        self.client_port = 14000
+        self.client_port = 12000
 
     def create_socket(self, adrss, port) -> socket:
         client_socket = socket(AF_INET, SOCK_STREAM)  
@@ -24,16 +26,22 @@ class Client():
 
     def connect(self):
         client_socket:socket = self.create_socket(self.client_address, self.client_port)
+        print(self.receive_data(client_socket))
         while True:
-            data = input("Ingrese string a revertir:")
-            if data!='':
+            data = input("Ingrese comando:")
+            if data == 'QUIT':
+                self.send_data(client_socket, data)
+                print(self.receive_data(client_socket))
+                print("Cerrando cliente...bye")
+                self.close_socket(client_socket)
+                break 
+            else:
+                print("Mensaje enviado, esperando server...")
                 self.send_data(client_socket,data)
                 received_data = self.receive_data(client_socket)
-                if (received_data!=''):
-                    print(f"Mensaje recibido: {received_data}")
-                else:
-                    print("Mensaje enviado, esperando server...")
+                print(f"\nRespuesta server:",received_data)
                     
+     
 if __name__ == '__main__':
     client = Client()
     client.connect()
--- servidorTCP_redes2021.py
+++ servidorTCP_redes2021.py
@@ -1,10 +1,15 @@
+from posix import listdir
+from posixpath import join
 from socket import *
+import os
+from os.path import isfile
 
 class Server():
 
     def __init__(self):
         self.server_address = 'localhost'
-        self.server_port = 14000
+        self.server_port = 12000
+        self.current_files=[]
 
     def create_socket(self, adrss, port) -> socket:
         server_socket = socket(AF_INET, SOCK_STREAM)  
@@ -18,6 +23,7 @@ class Server():
         return message
 
     def send_data(self, data:str, socket:socket):
+        data = str(data)
         socket.send(data.encode())
 
     def close_socket(self, socket:socket):
@@ -26,20 +32,57 @@ class Server():
     def connect(self):
         server_socket:socket = self.create_socket(self.server_address, self.server_port)      
         client_socket, address = server_socket.accept()
+        self.send_data(f'Conectado al server en {self.server_address, self.server_port}',client_socket)
         print(f"Conexion establecida con exito en {address}")
         while True:
             print("Esperando al cliente")
             data = self.receive_data(client_socket)
             if(data!=''):
-                if(data == 'quit'):
-                    self.send_data('Server finalizado...',client_socket)
-                    self.close_socket(client_socket)
-                    print("Cerrando server...Bye")
+                print(f"Mensaje cliente: {data}")
+                if data == 'QUIT':
+                    print("Cerrando server...bye!")
+                    self.send_data('Servidor finalizado',client_socket)
+                    self.close_socket(server_socket)
                     break
+                if data in self.cases:
+                    try:
+                        self.send_data(self.cases[data](self),client_socket)
+                    except KeyError:
+                        print("KeyError! ")
                 else:
-                    print(f"Mensaje recibido: {data}")
-                    self.send_data(data,client_socket)
-                    print("Respuesta enviada!")
+                    self.send_data(f"Comando no valido!\nLista de comandos: {list(self.cases)}",client_socket)
+            else:
+                self.send_data('Ingreso un valor!', client_socket)
+        
+    def list_method(self):
+        #Ruta de la direccion actual.
+        path = os.path.dirname(os.path.realpath(__file__))
+
+        #Lista de los archivos en esta ruta, los guardo por si los necesito mas luego.
+        
+        dir =[obj for obj in listdir(path) if isfile(join(path , obj))]
+        self.current_files = dir
+
+        print(f"Directory files: {dir}")
+
+        files = 'Archivos en el directorio:\n'
+
+        for file in dir:
+
+            files = files + str(file) +'\n'
+            
+        return files
+
+    #Auxiliar function for tests
+    def invert(self,data):
+        return data[::-1]
+ 
+    cases = {
+        
+    'LIST': list_method,
+    'QUIT': None
+     
+    }
             
 
 if __name__ == '__main__':
